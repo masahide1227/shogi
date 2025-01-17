af58e1bf45cfc56f4bed203cea351388136cbc82
masahide1227 <masahide.yabu@gmail.com>
2025-01-17 21:26:03 +0900

    バリデーションまで完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  validates :battle_type, presence: true, length: { maximum: 20 }
+  validates :game_recode, presence: true, length: { maximum: 1000 }
+  validates :text, presence: true, length: { maximum: 1000 }
+
     def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
     end
